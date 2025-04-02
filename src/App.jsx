451da4c3f9141775719bc1f91f7ee26e0e70f2d6
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-04-02 15:35:08 +0700

    neth-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 function index() {
   return (
     <div>
       <Navbar />
+      <Footer/>
     </div>
   );
 }
