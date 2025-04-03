38aa68df16e61534e3fecce25c8a7edd2cbd9e80
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-04-03 15:51:58 +0700

    neth uppdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,28 @@
-import Button from "./components/button";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "./page/Home/home";
+import Favorite from "./page/favorite";
+import Story from "./page/story";
+import Login from "./page/login";
+import Register from "./page/register";
 
-function index() {
+function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <Navbar />
-      <Footer/>
-    </div>
-=======
     <>
-      <div>
-        <Navbar />
-      </div>
+      <Navbar />
+      <main className="min-h-screen px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/story" element={<Story />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
->>>>>>> main
   );
 }
 
-export default index;
-
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   );
-// }
-
-// export default App;
+export default App;
